a9ff6fb96cdf46741836f578c3d33de2726b8a9f
brvdboss <bruno.vandenbossche@gmail.com>
2020-11-10 21:49:23 +0100

    Extend support for more video file formats. #4 Using ffmpeg to extract the initial frame in stead of relying what imageMagick is offering out of the box.

--- src/scripts/monitorandpublish-video.sh
+++ src/scripts/monitorandpublish-video.sh
@@ -11,7 +11,7 @@ tempident="/tmp/ident-videos"
 mkdir -p $tempident
 
 #Find all media image files
-extensions="avi mov mp4 vob mpg mpeg wmv webm"
+extensions="avi mov mp4 vob mpg mpeg wmv webm m2ts"
 rm /tmp/movethem-video.sh
 rm /tmp/batch-video
 for i in $extensions;
@@ -22,8 +22,19 @@ done
 while read line
 do
 	# We run the identify on the first frame
-	id=`identify -format %# "$line[0]"`
+	# extract the first frame with ffmpeg as this has the most extensive support
+	# for video formats.
+	#clean up any previous frame0 to avoid it being picked up for the wrong file
+	rm /tmp/frame0.bmp
+	ffmpeg -i $line -vframes 1 /tmp/frame0.bmp
+	#run the identify command on the bmp
+	#this basically generates the same output as:
+	#id=`identify -format %# "$line[0]"`
+	#however, going through ffmpeg we remove the need to have an appropriate delegate
+	#compiled in with imagemagick
+	id=`identify -format %# /tmp/frame0.bmp
 	ext=`bash -c 'echo ".${1##*.}"' _ "$line"`
+	rm /tmp/frame0.bmp
 	echo "id = $id"
 	echo "ext = $ext"
 	echo "mv -n \"$line\" $tempident/$id$ext" >> /tmp/movethem-video.sh
